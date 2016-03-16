20a396ca854541e57d003c62025e26781ea3fffe
tykesson89 <tykesson89@gmail.com>
2016-03-16 11:33:25 +0100

    Går att skapa användare

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -6,13 +6,14 @@ import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.sql.*;
+import java.util.HashMap;
 
 
 /**
  * Created by Henrik on 2016-03-15.
  */
 public class Server extends Thread {
-
+    private HashMap<String, String> map;
     private Connection conn;
     private ServerSocket serversocket;
     private Thread thread = new Thread(this);
@@ -28,29 +29,40 @@ public class Server extends Thread {
     public void run() {
         System.out.println("Server startad");
             try {
+                while(true){
+                String tag;
                 System.out.println("Waiting for client to connect!");
                 Socket socket = serversocket.accept();
                 ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
                 ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-
+                    new HashMap<String, String>();
                     String url = "jdbc:mysql://localhost:3306/autowork";
                     String user = "root";
-                    String password = "lösenord";
+                    String password = "hejhej";
                     Statement st = null;
+
                     try {
+                        tag = ois.readObject().toString();
+                        if(tag.equals("Create User")) {
+                            map = (HashMap) ois.readObject();
+                            String firstname = map.get("Firstname");
+                            String lastname = map.get("Lastname");
+                            String email = map.get("Email");
+                            String userPassword = map.get("Password");
+                            String salary = map.get("HourlyWage");
+                            String companyName = map.get("CompanyName");
+                            conn = DriverManager.getConnection(url, user, password);
+                            st = conn.createStatement();
+                            st.executeUpdate("INSERT INTO Users(Firstname, Lastname, Email, Password, Salary, Workplace) VALUES('"+firstname+"','"+ lastname+"','"+ email+"','"+ userPassword+"','"+ salary+"','"+ companyName+"' );");
 
-                        conn = DriverManager.getConnection(url, user,password);
-                        st = conn.createStatement();
-                        st.executeUpdate("INSERT INTO tablename(row1,row) VALUES('value1', 'value2');");
-
-
+                        }
                     } catch (SQLException ex) {
 
                         System.out.println("SQLException: " + ex.getMessage());
                         System.out.println("SQLState: " + ex.getSQLState());
                         System.out.println("VendorError: " + ex.getErrorCode());
                     }
-               // }
+               }
 
             } catch (Exception e) {
 
